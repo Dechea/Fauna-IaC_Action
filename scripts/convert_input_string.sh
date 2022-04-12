1e76fea0a1f1790f2491b47448215cb7df040c3e
Manuel Bauer <manuel.bauer@dechea.com>
2022-04-12 21:51:00 +0200

    Added split for @ sign III

--- scripts/convert_input_string.sh
+++ scripts/convert_input_string.sh
@@ -10,9 +10,13 @@ INPUT_LIST="$1,"
 SED_STRING=$2
 INPUT_LIST_MODIFIED=$(sed "$SED_STRING" <<< "$INPUT_LIST")
 # Change special characters
-# Needed for multi-line variable in github actions
-INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//'%'/'%25'}"
-INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\n'/'%0A'}"
-INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\r'/'%0D'}"
+## Needed for multi-line variable in github actions
+if [[ -z "$3" ]] ; then
+    INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//'%'/'%25'}"
+    INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\n'/'%0A'}"
+    INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\r'/'%0D'}"
+else
+  INPUT_LIST_MODIFIED="$( awk -F "$3" '{print $1}' <<< "$INPUT_LIST_MODIFIED" )"
+fi
 
 echo "$INPUT_LIST_MODIFIED"
