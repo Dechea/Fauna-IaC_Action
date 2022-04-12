d9c848097faa869f8634d94fc55db01b7f1f82d9
Manuel Bauer <manuel.bauer@dechea.com>
2022-04-13 00:10:03 +0200

    Added if statement for input string VIII

--- scripts/convert_input_string.sh
+++ scripts/convert_input_string.sh
@@ -9,12 +9,14 @@ fi
 INPUT_LIST="$1,"
 SED_STRING=$2
 INPUT_LIST_MODIFIED=$(sed "$SED_STRING" <<< "$INPUT_LIST")
-# Change special characters
-## Needed for multi-line variable in github actions
+
+# Remove @Ref if present
 if [[ -n "$3" ]] ; then
   INPUT_LIST_MODIFIED="$( awk -F "$3" '{print $1}' <<< "$INPUT_LIST_MODIFIED" )"
 fi
 
+# Needed for multi-line variable in github actions
+# Change special characters
 INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//'%'/'%25'}"
 INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\n'/'%0A'}"
 INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\r'/'%0D'}"
