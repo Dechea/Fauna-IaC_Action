4a7b449c55604292ff364ab441f313388aeea091
Manuel Bauer <manuel.bauer@dechea.com>
2022-04-12 23:57:05 +0200

    Added if statement for input string VII

--- scripts/convert_input_string.sh
+++ scripts/convert_input_string.sh
@@ -11,12 +11,12 @@ SED_STRING=$2
 INPUT_LIST_MODIFIED=$(sed "$SED_STRING" <<< "$INPUT_LIST")
 # Change special characters
 ## Needed for multi-line variable in github actions
-if [[ -z "$3" ]] ; then
-    INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//'%'/'%25'}"
-    INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\n'/'%0A'}"
-    INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\r'/'%0D'}"
-else
+if [[ -n "$3" ]] ; then
   INPUT_LIST_MODIFIED="$( awk -F "$3" '{print $1}' <<< "$INPUT_LIST_MODIFIED" )"
 fi
+
+INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//'%'/'%25'}"
+INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\n'/'%0A'}"
+INPUT_LIST_MODIFIED="${INPUT_LIST_MODIFIED//$'\r'/'%0D'}"
 
 echo "$INPUT_LIST_MODIFIED"
