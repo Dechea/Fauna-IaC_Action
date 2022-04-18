1f0c70a4beee3bec47d05629ac947bf40b53b776
Manuel Bauer <manuel.bauer@dechea.com>
2022-04-18 17:44:06 +0200

    added check and receive for correct fauna urls

---
+++ scripts/get_correct_fauna_domains.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+db_domain="db.fauna.com"
+graphql_domain="graphql.fauna.com"
+
+case "$1" in
+  "eu")
+    db_domain="db.eu.fauna.com"
+    graphql_domain="graphql.eu.fauna.com"
+    ;;
+  "us")
+    db_domain="db.us.fauna.com"
+    graphql_domain="graphql.us.fauna.com"
+    ;;
+  "preview")
+    db_domain="db.fauna-preview.com"
+    graphql_domain="graphql.fauna-preview.com"
+    ;;
+esac
+
+echo "$db_domain:$graphql_domain"
